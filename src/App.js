bba111c5992cb3e85934d7d35b9d880b27d34e60
normRoa <normanroam@gmail.com>
2020-05-07 13:20:57 -0700

    Adding a NavBar to the App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { GlobalStyle } from './styles/GlobalStyles'
-import { Logo } from './components/Logo'
-import { Home } from './pages/Home'
-import { Detail } from './pages/Detail'
-import { Router } from '@reach/router'
+import React from "react";
+import { GlobalStyle } from "./styles/GlobalStyles";
+import { Logo } from "./components/Logo";
+import { NavBar } from "./components/NavBar";
+import { Home } from "./pages/Home";
+import { Detail } from "./pages/Detail";
+import { Router } from "@reach/router";
 
 export const App = () => {
   return (
@@ -11,10 +12,11 @@ export const App = () => {
       <GlobalStyle />
       <Logo />
       <Router>
-        <Home path='/' />
-        <Home path='/pet/:categoryId' />
-        <Detail path='/detail/:detailId' />
+        <Home path="/" />
+        <Home path="/pet/:categoryId" />
+        <Detail path="/detail/:detailId" />
       </Router>
+      <NavBar />
     </div>
-  )
-}
+  );
+};
