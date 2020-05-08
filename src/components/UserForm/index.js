38cedadcef6947b28ef15f326ddc2fc812b4e7e8
normRoa <normanroam@gmail.com>
2020-05-07 19:16:37 -0700

    Handling Error (signup) and Loading state

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,23 +1,24 @@
 import React, { Fragment } from "react";
 import { useInputValue } from "../../hooks/useInputValue";
-import { Form, Input, Button, Title } from "./styles";
-export const UserForm = ({ onSubmit, title }) => {
+import { Form, Input, Button, Title, Error } from "./styles";
+export const UserForm = ({ error, disabled, onSubmit, title }) => {
   const email = useInputValue("");
   const password = useInputValue("");
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    onSubmit({ email: email.value, password: password.value })
-  }
+    e.preventDefault();
+    onSubmit({ email: email.value, password: password.value });
+  };
 
   return (
     <Fragment>
-      <Title>{title}</Title>
-      <Form onSubmit={handleSubmit}>
-        <Input placeholder="Email" {...email}></Input>
-        <Input placeholder="Password" type="password" {...password}></Input>
-        <Button>{title}</Button>
+      <Form disabled={disabled} onSubmit={handleSubmit}>
+        <Title>{title}</Title>
+        <Input disabled={disabled} placeholder="Email" {...email}></Input>
+        <Input disabled={disabled} placeholder="Password" type="password" {...password}></Input>
+        <Button disabled={disabled}>{title}</Button>
       </Form>
+      {error && <Error>{error}</Error>}
     </Fragment>
   );
 };
