0ae542a03d9ded42e295ca8582dd77674a13b87e
normRoa <normanroam@gmail.com>
2020-05-07 18:37:56 -0700

    Register Mutation (GraphQL)

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -4,10 +4,16 @@ import { Form, Input, Button, Title } from "./styles";
 export const UserForm = ({ onSubmit, title }) => {
   const email = useInputValue("");
   const password = useInputValue("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onSubmit({ email: email.value, password: password.value })
+  }
+
   return (
     <Fragment>
       <Title>{title}</Title>
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={handleSubmit}>
         <Input placeholder="Email" {...email}></Input>
         <Input placeholder="Password" type="password" {...password}></Input>
         <Button>{title}</Button>
