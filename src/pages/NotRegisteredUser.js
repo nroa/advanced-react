837331046f3c46b6d181fca294fdc9b1d934d05e
normRoa <normanroam@gmail.com>
2020-05-16 17:46:29 -0700

    Login Mutation

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,27 +1,54 @@
 import React, { Fragment } from "react";
 import Context from "../Context";
 import { UserForm } from "../components/UserForm";
-import { RegisterMutation } from '../container/RegisterMutation'
+import { RegisterMutation } from "../container/RegisterMutation";
+import { LoginMutation } from "../container/LoginMutation";
 export const NotRegisteredUser = () => (
   <Context.Consumer>
     {({ activateAuth }) => {
       return (
         <Fragment>
           <RegisterMutation>
-            {
-              (register, { data, loading, error }) => {
-                const onSubmit = ({ email, password }) => {
-                  const input = { email, password }
-                  const variables = { input }
-                  register({ variables }).then(activateAuth)
-                }
+            {(register, { data, loading, error }) => {
+              const onSubmit = ({ email, password }) => {
+                const input = { email, password };
+                const variables = { input };
+                register({ variables }).then(activateAuth);
+              };
 
-                const errorMsg = error && 'El usuario ya existe o hay algún problema.'
-                return <UserForm disabled={loading} error={errorMsg} title="Registrarse" onSubmit={onSubmit} />
-              }
-            }
+              const errorMsg =
+                error && "El usuario ya existe o hay algún problema.";
+              return (
+                <UserForm
+                  disabled={loading}
+                  error={errorMsg}
+                  title="Registrarse"
+                  onSubmit={onSubmit}
+                />
+              );
+            }}
           </RegisterMutation>
-          <UserForm title="Iniciar sesión" onSubmit={activateAuth} />
+
+          <LoginMutation>
+            {(login, { data, loading, error }) => {
+              const onSubmit = ({ email, password }) => {
+                const input = { email, password };
+                const variables = { input };
+                login({ variables }).then(activateAuth);
+              };
+
+              const errorMsg =
+                error && "La contraseña no es correcta o el usuario no existe.";
+              return (
+                <UserForm 
+                  title="Iniciar sesión" 
+                  onSubmit={onSubmit} 
+                  error={errorMsg}
+                  disabled={loading}
+                  />
+              );
+            }}
+          </LoginMutation>
         </Fragment>
       );
     }}
